b6af037e77b9f0c77b8c3e797ca4fe8d95d54586
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2020-03-31 13:06:05 +0200

    v1.1

---
+++ js/inline-script.js
@@ -0,0 +1,25 @@
+jQuery( function( $ ) {
+	$( '.comment-form' ).each( function() {
+		var $this          = $( this ),
+			$form_elements = $this.find( '> *:not(.comment-form-rating,.comment-notes)' ),
+			// Store rating element for live triggers.
+			$rating        = $this.find( '.comment-form-rating' ),
+			$rating_select = $rating.find( 'select' );
+
+		if ( ! $rating_select.length ) {
+			return true;
+		}
+
+		$form_elements.hide();
+
+		$rating_select.on( 'change', function() {
+			$form_elements.show();
+		} );
+
+		// Trigger from rating parent div element since stars are not present at load time.
+		// Cannot trigger from comment form as reCaptcha deletes all events from this element.
+		$rating.on( 'click touchend', 'a', function( e ) {
+			$form_elements.show();
+		} );
+	} );
+} );
---
+++ woo-review-form-hide.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Reviews - Hide Form
+ * Description: Hides the Woocommerce review form until user selects a rating
+ * Version:     1.1
+ * Author:      Jory Hogeveen
+ * Author URI:  https://www.keraweb.nl
+ */
+
+if ( ! defined ( 'ABSPATH' ) ) {
+	die;
+}
+
+Woo_Review_Hide_Form::get_instance();
+
+class Woo_Review_Hide_Form
+{
+	/**
+	 * @var Woo_Review_Hide_Form
+	 */
+	private static $_instance = null;
+
+	/**
+	 * @return Woo_Review_Hide_Form
+	 */
+	public static function get_instance() {
+		if ( ! self::$_instance ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	/**
+	 * Woo_Review_Hide_Form constructor.
+	 */
+	protected function __construct() {
+		add_action( 'wp_footer', __CLASS__ . '::script' );
+	}
+
+	/**
+	 * Render inline script.
+	 */
+	public static function script() {
+		if ( ! did_action( 'comment_form' ) ) {
+			return;
+		}
+		if ( ! function_exists( 'wc_review_ratings_enabled' ) || ! wc_review_ratings_enabled() ) {
+			return;
+		}
+
+		echo '<script id="woo-review-hide-form">';
+		include 'js/inline-script.js';
+		echo '</script>';
+	}
+}
